d233cf16f29b96900af0ed78bcb66408a0ac67f9
nataliosaleky <nataliosaleky@gmail.com>
2020-11-23 13:19:55 +0700

    Booking UI Edited

--- app/src/main/java/com/example/interoperabilitas/BookingActivity.java
+++ app/src/main/java/com/example/interoperabilitas/BookingActivity.java
@@ -15,20 +15,30 @@ import android.widget.Toast;
 public class BookingActivity extends AppCompatActivity {
     TextView waktuMasuk;
     TextView waktuSelesai;
+//    int masuk = Integer.parseInt(waktuMasuk.getText().toString().substring(0,2));
+//    int selesai = Integer.parseInt(waktuSelesai.getText().toString().substring(0,2));
     Context context;
 
-
+//    public void onDisplay(View v){
+//        if(masuk>=selesai){
+//            Toast.makeText(BookingActivity.this, "Invalid time format", Toast.LENGTH_SHORT).show();
+//        }
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
         waktuMasuk = (TextView) findViewById(R.id.BookingWaktuMasuk);
+        waktuSelesai = (TextView) findViewById(R.id.BookingWaktuSelesai);
         context = this;
+
+
         waktuMasuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Dialog dialog = new Dialog(context);
                 dialog.setContentView(R.layout.dialog_booking);
+                dialog.show();
                 Button button10 = (Button)dialog.findViewById(R.id.button10);
                 Button button11 = (Button)dialog.findViewById(R.id.button11);
                 Button button12 = (Button)dialog.findViewById(R.id.button12);
@@ -125,19 +135,116 @@ public class BookingActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
-                dialog.show();
+
 
             }
         });
 
-//        final TextView period1Text = (TextView) findViewById(R.id.Period1TextView);
-//        period1Text.setTextColor(Color.parseColor("#000000"));
-//        final TextView period3Text = (TextView) findViewById(R.id.Period3TextView);
-//        period3Text.setTextColor(Color.parseColor("#000000"));
-    }
-//    public void openDialog(){
-//        BookingDialog exampleDialog = new BookingDialog();
-//        exampleDialog.show(getSupportFragmentManager(), "booking dialog");
-//    }
+        waktuSelesai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog_booking2);
+                dialog.show();
+                Button button11 = (Button)dialog.findViewById(R.id.button11);
+                Button button12 = (Button)dialog.findViewById(R.id.button12);
+                Button button13 = (Button)dialog.findViewById(R.id.button13);
+                Button button14 = (Button)dialog.findViewById(R.id.button14);
+                Button button15 = (Button)dialog.findViewById(R.id.button15);
+                Button button16 = (Button)dialog.findViewById(R.id.button16);
+                Button button17 = (Button)dialog.findViewById(R.id.button17);
+                Button button18 = (Button)dialog.findViewById(R.id.button18);
+                Button button19 = (Button)dialog.findViewById(R.id.button19);
+                Button button20 = (Button)dialog.findViewById(R.id.button20);
+                Button button21 = (Button)dialog.findViewById(R.id.button21);
+                Button button22 = (Button)dialog.findViewById(R.id.button22);
 
+                button11.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("11:00");
+                        dialog.dismiss();
+                    }
+                });
+                button12.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("12:00");
+                        dialog.dismiss();
+                    }
+                });
+                button13.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("13:00");
+                        dialog.dismiss();
+                    }
+                });
+                button14.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("14:00");
+                        dialog.dismiss();
+                    }
+                });
+                button15.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("15:00");
+                        dialog.dismiss();
+                    }
+                });
+                button16.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("16:00");
+                        dialog.dismiss();
+                    }
+                });
+                button17.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("17:00");
+                        dialog.dismiss();
+                    }
+                });
+                button18.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("18:00");
+                        dialog.dismiss();
+                    }
+                });
+                button19.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("19:00");
+                        dialog.dismiss();
+                    }
+                });
+                button20.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("20:00");
+                        dialog.dismiss();
+                    }
+                });
+                button21.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("21:00");
+                        dialog.dismiss();
+                    }
+                });
+                button22.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        waktuSelesai.setText("22:00");
+                        dialog.dismiss();
+                    }
+                });
+
+            }
+        });
+    }
 }
